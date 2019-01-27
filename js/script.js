c5c33e61ae8ee8a20a2b164a49680bf59cd28f98
RyanDWelch <rdw1809@gmail.com>
2019-01-27 06:43:51 -0800

    Giant update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,175 @@
 $('document').ready(function(){
   // add current year to footer
-  var currentYear = new Date().getFullYear();
-  $('#uCYear').text(currentYear);
+  $('#cYear').text(new Date().getFullYear());
 
-  // add 100 items to grid container
+  // add grid-items to grid-container
   $('#grid-container').each(function(){
-    var gridItems = "";
-    for(i=1;i<=100;i++){
-      if (i == 50) {
-        gridItems += '<div class="grid-item"><span><i class="fa fa-star"></i></span></div>';
-      } else {
-        gridItems += '<div class="grid-item"><span>'+i+'</span></div>';
+    var gridItems = '';
+    for(i=1;i<=110;i++){
+      switch (i) {
+        case 1:
+          // hideshow
+          gridItems += '<div class="grid-item grid-button hideshow" data-griditem="' + i;
+          gridItems += '"><span><i class="fa fa-toggle-on"></i><i class="fa fa-toggle-off"></i></span></div>';
+          break;
+        case 12:
+          // dark mode
+          gridItems += '<div class="grid-item grid-button darkmode" data-griditem="' + i;
+          gridItems += '"><span id="dmode"><i class="fa fa-moon-o"></i><span>dark mode</span>';
+          gridItems += '</span><span id="lmode"><i class="fa fa-sun-o"></i><span>light mode</span></span></div>';
+          break;
+        case 23:
+          // shownums
+          gridItems += '<div class="grid-item grid-button shownums" data-griditem="' + i;
+          gridItems += '"><span><i class="fa fa-hashtag"></i></span></div>';
+          break;
+        case 34:
+          // showgrid
+          gridItems += '<div class="grid-item grid-button showgrid" data-griditem="' + i;
+          gridItems += '"><span><i class="fa fa-th"></i></span></div>';
+          break;
+        case 45:
+          // countdown
+          gridItems += '<div class="grid-item grid-button countdown" data-griditem="' + i;
+          gridItems += '"><span><i class="fa fa-clock-o"></i></span></div>';
+          break;
+        case 56:
+          // fadeinout
+          gridItems += '<div class="grid-item grid-button fadeinout" data-griditem="' + i;
+          gridItems += '"><span><i class="fa fa-bullseye"></i></span></div>';
+          break;
+        case 89:
+          // primary-color
+          gridItems += '<div class="grid-item grid-button primary-color" data-griditem="' + i;
+          gridItems += '"><input type="color" value="#000"></div>';
+          break;
+        case 78:
+          // secondary-color
+          gridItems += '<div class="grid-item grid-button secondary-color" data-griditem="' + i;
+          gridItems += '"><input type="color" value="#ff0000"></div>';
+          break;
+        case 100:
+          // tertiary-color
+          gridItems += '<div class="grid-item grid-button tertiary-color" data-griditem="' + i;
+          gridItems += '"><input type="color" value="#ffffff"></div>';
+          break;
+        case 67:
+          // clear
+          gridItems += '<div class="grid-item grid-button clear" data-griditem="' + i;
+          gridItems += '"><span><i class="fa fa-ban"></i></span></div>';
+          break;
+        default:
+          gridItems += '<div class="grid-item" data-griditem="' + i + '"><span>' + i + '</span></div>';
       }
     }
     $(this).append(gridItems);
   });
 
-
+  // mobile hamburger menu functionality
   $('.mobile-menu').click(function(){
     $(this).toggleClass('open');
   });
 
+  // add navigation links content hide/display functionality
   $('.nav-item').click(function(e){
     e.preventDefault();
-    var pageName = $(this).attr('data-nav');
-    $('body').removeClass().addClass(pageName);
+    $('body').removeAttr('data-page').attr('data-page',$(this).attr('data-nav'));
     $('.mobile-menu').removeClass('open');
     $('.nav-item').removeClass('active');
     $(this).addClass('active');
   });
 
+  // generic addButton function for basic css based 'features'
+  function addButton(selector,featureName){
+    $(selector).click(function(){
+      if ($(this).hasClass('clicked')){
+        $('body').removeClass(featureName);
+        $(this).removeClass('clicked');
+      } else {
+        $('body').addClass(featureName);
+        $(this).addClass('clicked');
+      }
+    });
+  }
+  // hide/show
+  addButton('.hideshow','hidecontent');
+  // shownums
+  addButton('.shownums','shownums');
+  // showgrid
+  addButton('.showgrid','showgrid');
+  // fadeinout
+  addButton('.fadeinout','fadeinout');
+
+
+  // 'custom' buttons
+  // dark mode
+  $('.darkmode').click(function(){
+    if ($('body').hasClass('light')){
+      // light mode enabled, clear everything and turn dark mode on
+      $('.grid-item:not(.grid-button)').unbind().removeClass('darkened');
+      $('body > h1, footer > p, #main-nav > a').removeClass('darkened');
+      $('body').removeClass('light').addClass('dark');
+      $('.grid-item, body > h1, footer > p, #main-nav > a').hover(function(){
+        $(this).addClass('darkened');
+      });
+    } else {
+      // dark mode is enabled, turn off
+      $('body').removeClass('dark').addClass('light');
+      $('.grid-item, body > h1, footer > p, #main-nav > a').unbind('mouseenter mouseleave');
+      $('body *').removeClass('darkened');
+    }
+  });
+
+  // countdown
+  var count = 0;
+  function cycleGrid(){
+    var oneSecond = setTimeout(function(){
+      console.log('cycle count: '+count);
+      var selection = $('.grid-item:not(.grid-button) span');
+      if (count <= selection.length) {
+        if (selection.eq(count).css('opacity') == 1){
+          selection.eq(count).animate({opacity: 0}, 250);
+          selection.eq(count).animate({opacity: 1}, 250);
+          selection.eq(count).animate({opacity: 0}, 250);
+          selection.eq(count).animate({opacity: 1}, 250);
+        } else {
+          selection.eq(count).animate({opacity: 1}, 250);
+          selection.eq(count).animate({opacity: 0}, 250);
+          selection.eq(count).animate({opacity: 1}, 250);
+          selection.eq(count).animate({opacity: 0}, 250);
+        }
+        count++;
+        cycleGrid();
+      }
+    }, 1000);
+  }
+  $('.countdown').click(function(){
+    cycleGrid();
+  });
+
+  // clear
+  $('.clear').click(function(){
+      $('body').removeClass().addClass('light').attr('data-page','home');
+      $('body *').removeClass('darkened');
+      $('.grid-item, body > h1, footer > p, #main-nav > a').unbind('mouseenter mouseleave');
+      //remove styles
+      $('.grid-item span').removeAttr('style');
+      // reset colors
+      $('html').css({'--primary-color':'#000000','--secondary-color':'#ff0000','--tertiary-color':'#ffffff'});
+      $('.primary-color input').val('#000000');
+      $('.secondary-color input').val('#ff0000');
+      $('.tertiary-color input').val('#ffffff')
+
+  });
+
+  // colors
+  function colorBttns(s){
+    $('.'+s+' input').change(function(){
+      $('html').css('--'+s,$(this).val());
+    });
+  }
+  colorBttns('primary-color');
+  colorBttns('secondary-color');
+  colorBttns('tertiary-color');
 
 });
